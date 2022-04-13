3466608e3ee78e5cc7c2126da3362c448b280bc7
yassmine23 <yasminessam827@gmail.com>
2022-04-13 17:49:19 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,104 @@
+/*var card= ``
+for (var i=0 ; i<10 ; i++ )
+{
+    card+= `<div class="col-md-4"> 
+    <div class="item">
+        <img src="images/client-2.png" alt="client">
+        <h2>Abdallah Atef</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate nesciunt hic temporibus ullam! Commodi optio veniam iure provident, hic cum itaque sed, adipisci fugiat, repudiandae iusto. Id cumque dolor accusantium.</p>
+    </div>
+  </div>` ;
+}
+
+document.getElementById('demo').innerHTML= card;
+*/
+
+
+// دخل رقم يقبل السمة على ال3 وال4 
+/* var number= window.prompt('ENTER A NUMBER!');
+
+if (number %3 ==0 && number%4==0)
+{ document.write("true");
+}
+else
+{
+    document.write("false");
+} 
+*/
+
+//دخل رقمين ويطبع الرقم الاكبر 
+/* var number1=Number( window.prompt('ENTER A NUMBER!'));
+var number2=Number( window.prompt('ENTER A NUMBER!'));
+if(number1> number2)
+{document.write(number1);
+}
+else{
+    document.write(number2);
+}
+*/
+
+//اليوزر يدخل رقم لو بالساب يكتبلي نيجاتف لو بالوجب يكتبلي بوزنف
+/*var number= Number(prompt("ENTER A NUMBER!"));
+if ( number<0 )
+{
+    document.write ("it's Negative");
+}
+else{
+    document.write("it's POSITIVE ");
+}
+*/
+
+//ادخل 3 ارقام واطلع الmax & min بينهم
+ /*var number1= Number(prompt("ENTER A NUMBER!"));
+var number2= Number(prompt("ENTER A NUMBER!"));
+var number3= Number(prompt("ENTER A NUMBER!"));
+
+let minimum = Math.min(number1,number2, number3);
+document.write("minimums num is:"+ minimum);
+*/
+
+//برنامج يعرف اذا كان الرقم زوجي ولا فردي
+/*var number= Number(prompt("ENTER A NUMBER!"));
+if(number%2 == 0)
+{
+    document.write("it's oven!");
+}
+else{
+    document.write("it's odd!");
+}
+*/
+
+//اكتب برنامج يكتب الارقام من 0 لحد الرقم اللي ندخله
+/* var number= Number(prompt("ENTER A NUMBER!"));
+for(var i=1; i<=number ; i++ )
+{
+document.write(i+",");
+}
+*/
+
+//اكتب برنامج بيدخل رقم وبعدين يطبع جدول ضرب الرقم ده * 12 
+/*var number= Number(prompt("ENTER A NUMBER!"));
+var result;
+for(var i=1; i<=12; i++)
+{
+    result= number*i;
+    document.write(result);
+}
+*/
+
+//function 
+/*
+var x= Number(prompt("enter first number"));
+var y= Number(prompt("enter second number"));
+
+function getAvr (num1, num2){
+    var sum= num1+ num2;
+    var result= sum/2 ;
+    document.write(result);
+}
+getAvr(x, y)
+*/
+
+// welcome function
+var name= window.prompt("enter your name:");
+function 
